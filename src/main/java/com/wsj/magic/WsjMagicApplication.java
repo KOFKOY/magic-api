d0c7f88031dd011dfb6e20858f94a7668e6295d7
wsj <1838039390@qq.com>
2025-01-16 15:28:47 +0800

    init

---
+++ src/main/java/com/wsj/magic/WsjMagicApplication.java
@@ -0,0 +1,16 @@
+package com.wsj.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.HashMap;
+
+@SpringBootApplication
+public class WsjMagicApplication {
+
+    public static void main(String[] args) {
+        
+        SpringApplication.run(WsjMagicApplication.class, args);
+    }
+
+}
---
+++ src/main/resources/magic-editor.js
@@ -0,0 +1,12 @@
+var MAGIC_EDITOR_CONFIG = {
+    title: 'Magic API',
+    header: {
+        skin: true,    // 屏蔽皮肤按钮
+        document: true,    // 屏蔽文档按钮
+        repo: true,    // 屏蔽gitee和github
+        qqGroup: false  // 屏蔽加入QQ群
+    },
+    decorationTimeout: 15000,//编辑器错误提示显示时长，< 0为一直显示，单位为毫秒，默认10秒
+    defaultExpand: false //用于控制左侧树是否默认展开
+    // 其它配置参考本页中其它配置项
+}
